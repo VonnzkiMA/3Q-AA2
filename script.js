5b1354a21c96b7a721bcedaf2bae229770de8107
VonnzkiMA <rvfmiguel22@src.pshs.edu.ph>
2025-03-07 11:25:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function finalPopulation() {
-	let initialPopulation = prompt("intial population: ");
-	let rateOfGrowth = prompt("rate of growth: ");
-	let timeInHours = prompt("time in hours: ");
+	let initialPopulation = prompt("Input intial population: ");
+	let rateOfGrowth = prompt("Input rate of growth: ");
+	let timeInHours = prompt("Input time in hours: ");
+	
+	
 	
 	let floatInitialPopulation = parseFloat(initialPopulation);
 	let floatRateOfGrowth = parseFloat(rateOfGrowth);
@@ -9,8 +11,8 @@ function finalPopulation() {
 	
 	let finalPopulation = Math.round(floatInitialPopulation * Math.pow(Math.E, floatRateOfGrowth * floatTimeInHours));
 			
-	let regionOfMonster = prompt("region of the monster: ");
-	let nameOfMonster = prompt("name of the monster: ");
+	let regionOfMonster = prompt("Input region of the monster: ");
+	let nameOfMonster = prompt("Input name of the monster: ");
 	let monster = (regionOfMonster.concat(' ').concat(nameOfMonster)).toUpperCase();
 	
 	document.getElementById('result').innerHTML = ('After' + ' ' + floatTimeInHours + ' ' + 'hours' + ' ' + 'the population of' + ' ' + monster + ' ' + 'has risen to' + ' ' + finalPopulation + '!');
